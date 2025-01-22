696afce695da502af158116dda2c36b7fa1ba263
Insidious Fiddler <me@codycody31.dev>
2025-01-22 12:08:39 -0500

    feat: add linux support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:my_ai_gateway/pages/chat.dart';
 import 'package:my_ai_gateway/services/theme_notifier.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
 
 void main() {
+  if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+  }
+  // Change the default factory. On iOS/Android, if not using `sqlite_flutter_lib` you can forget
+  // this step, it will use the sqlite version available on the system.
+  databaseFactory = databaseFactoryFfi;
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeNotifier(),
